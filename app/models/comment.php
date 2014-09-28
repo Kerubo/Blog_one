4580705866632db12223bea2fcb0f85f9204be82
Kerubo <lucykerubo763@gmail.com>
2014-09-29 01:30:58 +0300

    My Update

--- app/models/comment.php
+++ app/models/comment.php
@@ -1,9 +1,14 @@
 <?php
-
+ //file: app/controllers/CommentsController
+ .php
+ // the info collectedfrom the db and returned bby the contollers  in thr#FFFFFF
 class Comment extends Eloquent {
  
 public function post()
 {
+	
 return $this->belongsTo('Post');
+
 }
+
 }
